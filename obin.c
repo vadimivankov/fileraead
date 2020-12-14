97f4018d0b4e85640da6208c078086af329a6868
vadimivankov <51121668+vadimivankov@users.noreply.github.com>
2020-12-14 20:08:09 +0300

    Update obin.c

--- obin.c
+++ obin.c
@@ -21,13 +21,13 @@
 следующим образом: t = t_us0 – dt; где t_us0 – текущее время во входном файле УС0.)
 
 Структура паспорта:
-1) 12 = Имя объекта (char)
-2) 2 = Индекс (unsigned int)
-3) 1 = Ф - формат объекта (unsigned int)
-4) 1 = Т - тип объекта (unsigned int)
-5) 4 = А0 - масштабный коэффициент (float)
-6) 4 = А1 - масштабный коэффициент (float)
-7) 8 = Размерность параметра (char)
+1) 12 (0...11) = Имя объекта (char)
+2) 2 (12...13) = Индекс (unsigned int)
+3) 1 (14) = Ф - формат объекта (unsigned int)
+4) 1 (15) = Т - тип объекта (unsigned int)
+5) 4 (16...19) = А0 - масштабный коэффициент (float)
+6) 4 (20...23) = А1 - масштабный коэффициент (float)
+7) 8 (24...31) = Размерность параметра (char)
 
 Форматы объектов следующие:
 0 - массив (кадр, строка) неопределенной длины,
@@ -104,10 +104,25 @@ unsigned int file_data_int(const char * filename, int position, int bytes)
 }
 
 
+void data_passport(const char * filename, int size_of_passport, int passport_position, int amount_passports)
+{
+    //printf("Passport position:\t%d\n", passport_position);
+    //printf("Amount of passports:\t%d\n", amount_passports);
+    cout << "\tName of subject:\t" << file_data_str(filename, passport_position, 12) << "\n";
+    cout << "\tIndex:\t\t\t" << file_data_int(filename, passport_position + 12, 2) << "\n";
+    cout << "\tSubject format:\t\t" << file_data_int(filename, passport_position + 14, 1) << "\n";
+    cout << "\tType of subject:\t" << file_data_int(filename, passport_position + 15, 1) << "\n";
+    cout << "\tA0:\t\t\t" << file_data_flt(filename, passport_position + 16, 4) << "\n";
+    cout << "\tA1:\t\t\t" << file_data_flt(filename, passport_position + 20, 4) << "\n";
+    cout << "\tAmount of parameter:\t" << file_data_str(filename, passport_position + 24, 8) << "\n\n\n";
+}
+
+
+
 int main()
 {
-    SetConsoleOutputCP(1251);
-//    SetConsoleCP(1251);
+    SetConsoleOutputCP(866);
+//    SetConsoleCP(866);
 
     const char * filename = "1.us0";
 
@@ -118,7 +133,6 @@ int main()
         return 1;
     }
     fclose (F);
-
 
     cout << "YC0 :\t\t\t" << file_data_str(filename, 0, 4) << "\n"; // "УС0 "
     cout << "Name of product:\t" << file_data_str(filename, 4, 8) << "\n"; // "Имя изделия"
@@ -131,52 +145,21 @@ int main()
     cout << "Comment length:\t\t" << file_data_int(filename, 39, 1) << "\n"; // "Длина комментария"
     cout << "\n";
 
-/*    char s[10] = "";
+// *************** PASSPORTS ***************
 
-    fread (s , 4 , 1 , F);
-    cout << "YC0 :\t\t\t" << s << endl;
-
-    fread (s , 8 , 1 , F);
-    cout << "Name of product:\t" << s << endl;
-
-    fread (s , 8 , 1 , F);
-    cout << "Name of test:\t\t" << s << endl;
-
-    fread (s , 8 , 1 , F);
-    cout << "Date of test:\t\t" << s << endl;
+    int size_of_passport = 32;
+    int passport_position = 40 + file_data_int(filename, 39, 1);
     
+    int amount_passports = file_data_int(filename, 36, 2);
 
+    //for (int i = 0; i < 5; i++)   // test of mechanic
+    for (int i = 0; i < amount_passports; i++)
+    {
+        printf("Data of %d  passport:\n", i + 1);
+        data_passport(filename, size_of_passport, passport_position, amount_passports);
+        passport_position += size_of_passport;
+    }
 
-    float f = .0;
-
-    fread (&f , 4 , 1 , F);
-    cout << "Initial test time:\t" << f << endl;
-    f = .0;
-
-    fread (&f , 4 , 1 , F);
-    cout << "End test time:\t\t" << f << endl;
-    f = .0;
-
-
-
-    unsigned int ui = 0;
-
-    fread (&ui , 2 , 1 , F);
-    cout << "Amount of passports:\t" <<  ui << endl;
-    ui = 0;
-
-    fread (&ui , 1 , 1 , F);
-    cout << "Kind of file:\t\t" <<  ui << endl;
-    ui = 0;
-
-    fread (&ui , 1 , 1 , F);
-    cout << "Comment length:\t\t" << ui << endl;
-    ui = 0;
-
-    fclose (F);
-
-*/
 
     return 0;
 }
-
